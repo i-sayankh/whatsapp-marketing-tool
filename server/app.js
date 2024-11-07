7b3f7a6528fec5075143aa7704aaffc2071dd7ad
sayank.khutia123@gmail.com <sayank.khutia123@gmail.com>
2024-11-07 21:17:30 +0530

    Set up Express Server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("MongoDB connection error:", err));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
